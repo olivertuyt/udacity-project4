ddfc174abeb64285e2c03ce8a6bd33af755b6d27
huypq <quanghuypham.95@gmail.com>
2022-10-25 22:07:22 +0700

    Log event

--- azure-vote/main.py
+++ azure-vote/main.py
@@ -104,11 +104,13 @@ def index():
             properties = {'custom_dimensions': {'Cats Vote': vote1}}
             # TODO: use logger object to log cat vote
             logger.info('Cats Vote', extra=properties)
+            logger.warning('Cats Vote', extra=properties)
 
             vote2 = r.get(button2).decode('utf-8')
             properties = {'custom_dimensions': {'Dogs Vote': vote2}}
             # TODO: use logger object to log dog vote
             logger.info('Cats Vote', extra=properties)
+            logger.warning('Cats Vote', extra=properties)
 
             return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
 
